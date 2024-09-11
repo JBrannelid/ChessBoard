9cfdd7adc2117ff88785e4151003a3b7b8289a45
brann <brann@JBRANNELID>
2024-09-11 13:07:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿// Programmer:   Johannes Brannelid
+// Program:      Fullstack .NET 2024
+// Course:       Programmering med C# och .NET
+// Lab 2:        Schackbräde
+
+namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            int inputNumber = 0;
+            string userBlackTile = "◻︎";
+            string userWhiteTile = "◼︎";
+            string userInput = "";
+            bool endApp = false;
+
+            Console.WriteLine($"Välkommen till ChessBoard!\n");
+            Console.Write("Hur stor vill du att chackbrädan ska vara?\n" +
+                          "Skriv en siffra mellan 1-10: ");
+            inputNumber = ReadInt.userTestInput(inputNumber);
+            Console.WriteLine("");
+
+            DrawBoard(inputNumber, userBlackTile, userWhiteTile);
+
+            while (!endApp)
+            {
+                printMenu();
+                userInput = Console.ReadLine().ToUpper();
+                Console.Clear();
+
+                switch (userInput)
+                {
+                    case "A":
+                        Console.Write("Hur stor vill du att chackbrädan ska vara?\n" +
+                                      "Skriv en siffra mellan 1-10: ");
+                        inputNumber = ReadInt.userTestInput(inputNumber);
+                        Console.WriteLine("");
+                        DrawBoard(inputNumber, userBlackTile, userWhiteTile);
+                        break;
+                    case "B":
+                        Console.WriteLine("Hur ska svarta rutor se ut?");
+                        userBlackTile = Console.ReadLine().ToUpper();
+                        Console.WriteLine("Hur ska vita rutor se ut?");
+                        userWhiteTile = Console.ReadLine().ToUpper();
+                        Console.WriteLine("");
+                        ChangeTiles(inputNumber,userBlackTile, userWhiteTile);
+                        break;
+                    case "C":
+                        endApp = true;
+                        Console.WriteLine("Välkommen tillbaka!");
+                        break;
+                    default:
+                        Console.Write("Fel inmatning, välj alternativ A, B eller C");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+        static void printMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Menyval");
+            Console.WriteLine("[A]Ändra chackbrädans storlek");
+            Console.WriteLine("[B]Ändra chackbrädans tecken");
+            Console.WriteLine("[C]Avsluta programmet");
+        }
+        static void DrawBoard(int inputNumber, string userBlackTile, string userWhiteTile)
+        {
+            string white = userBlackTile;
+            string black = userWhiteTile;
+
+            for (int i = 0; i < inputNumber; i++)
+            {
+                for (int j = 0; j < inputNumber; j++)
+                {
+                    Console.Write(((i + j) % 2 == 0) ? white : black);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void ChangeTiles(int inputNumber, string userBlackTile, string userWhiteTile)
+        {
+            {
+                string white = userBlackTile;
+                string black = userWhiteTile;
+
+                for (int i = 0; i < inputNumber; i++)
+                {
+                    for (int j = 0; j < inputNumber; j++)
+                    {
+                        Console.Write(((i + j) % 2 == 0) ? white : black);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+/* README
+ * The program uses 1 declared and 4 initialized variabels
+ * Variabel inputNumber call the class ReadInt and function userTestInput
+ * The function UserTestInput check if the input is an integer within choosen interval. If true, return the value
+ * 
+ * The main programfile (Program.cs) use three functions. PrintMenu, DrawBoard and changeTiles
+ * PrintMenu print out the menu for swith-statments
+ * DrawBoard and ChangeTiles print out the chessboard by a nested loop. i-loop counting rows, j-loop counting columns. 
+ * The nested loop continue as long as choosen inpuNumber choosen by the user
+ * A ternary operator "if-statment" in j-loop alternate the white/black square based on the sum of the row and column result
+*/
---
+++ ReadInt.cs
@@ -0,0 +1,35 @@
+﻿using ChessBoard;
+using System;
+
+namespace ChessBoard
+{
+    public static class ReadInt
+    {
+        public static int userTestInput(int failTestNumber)
+        {
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out failTestNumber))
+                {
+                    if (failTestNumber >= 1 && failTestNumber <= 10)
+                        return failTestNumber;
+                    else
+                        Console.WriteLine("Siffran är utanför det tillåtna intervallet");
+                }
+                else
+                {
+                    Console.WriteLine("Du skrev inte in ett heltal. Försök igen:");
+                }
+            }
+        }
+    }
+}
+/* README
+ * ReadInt method. Public static class so that the method can be called outside this class
+ * Convert userInput to an integer in an nested if-statment
+ * While-loop is set to true. If condition is meet, break out from the loop and return failTestNumber
+ * 
+ * Questions: Är det möjligt att retunera ett värde med ternary operator i console.writeline?
+ * ex:        console.writeline(num <= 1 && num <=19) ? return num : "försök igen");
+ *                                                      return blir error i körning 
+*/
